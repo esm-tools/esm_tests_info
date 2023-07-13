d55bcc4f4fd247c4d00cc446fcd2131a5f384fbb
mandresm <miguel.andres-martinez@awi.de>
2023-07-13 15:22:34 +0200

    awicm3 version 3.1 tests transferred to 3.1.1

--- last_tested/levante/comp/awicm3/awicm3-v3.1.1/comp-xios-2.5_script.sh
+++ last_tested/levante/comp/awicm3/awicm3-v3.1.1/comp-xios-2.5_script.sh
@@ -90,5 +90,5 @@ unset SLURM_NPROCS
 unset SLURM_ARBITRARY_NODELIST
 
 cd xios
-export XIOS_TOPLEVEL=<TEST_DIR>comp/awicm3/awicm3-v3.1/xios; ./make_xios --arch ESMTOOLS_levante_oasis_intel --netcdf_lib netcdf4_par --use_oasis oasis3_mct --job 24 --prod; cp bin/xios_server.exe bin/xios.x
+export XIOS_TOPLEVEL=<TEST_DIR>comp/awicm3/awicm3-v3.1.1/xios; ./make_xios --arch ESMTOOLS_levante_oasis_intel --netcdf_lib netcdf4_par --use_oasis oasis3_mct --job 24 --prod; cp bin/xios_server.exe bin/xios.x
 cd ..
