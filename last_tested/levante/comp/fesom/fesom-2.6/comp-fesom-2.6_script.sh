0d04f253d40b049dcac1380eadf17b34147ef018
mandresm <miguel.andres-martinez@awi.de>
2025-03-12 09:37:45 +0100

    updates in levante tests without environment_changes: fesom-2.6 and awicm3-v3.2

--- last_tested/levante/comp/fesom/fesom-2.6/comp-fesom-2.6_script.sh
+++ last_tested/levante/comp/fesom/fesom-2.6/comp-fesom-2.6_script.sh
@@ -11,7 +11,6 @@ module load openmpi/4.1.2-intel-2021.5.0
 module load netcdf-c/4.8.1-openmpi-4.1.2-intel-2021.5.0
 module load netcdf-fortran/4.5.3-openmpi-4.1.2-intel-2021.5.0
 module load hdf5/1.12.1-openmpi-4.1.2-intel-2021.5.0
-module load libaec/1.0.5-intel-2021.5.0
 
 export LC_ALL=en_US.UTF-8
 export CPU_MODEL=AMD_EPYC_ZEN3
@@ -39,7 +38,7 @@ export MPIROOT="$(mpif90 -show | perl -lne 'm{ -I(.*?)/include } and print $1')"
 export MPI_LIB="$(mpif90 -show |sed -e 's/^[^ ]*//' -e 's/-[I][^ ]*//g')"
 export IO_LIB_ROOT=/work/ab0246/HPC_libraries/intel-oneapi-compilers/2022.0.1-gcc-11.2.0/openmpi/4.1.2-intel-2021.5.0
 export HDF5ROOT=/sw/spack-levante/hdf5-1.12.1-tvymb5
-export HDF5_C_INCLUDE_DIRECTORIES=$HDF5_ROOT/include
+export HDF5_C_INCLUDE_DIRECTORIES=$HDF5ROOT/include
 export HDF5_ROOT=$HDF5ROOT
 export NETCDFFROOT=/sw/spack-levante/netcdf-fortran-4.5.3-k6xq5g
 export NETCDFROOT=/sw/spack-levante/netcdf-c-4.8.1-2k3cmu
@@ -56,46 +55,10 @@ export LD_LIBRARY_PATH=$HDF5ROOT/lib:$NETCDFROOT/lib:$NETCDFFROOT/lib:$LD_LIBRAR
 export LD_LIBRARY_PATH=/sw/spack-levante/intel-oneapi-mkl-2022.0.1-ttdktf/mkl/2022.0.1/lib/intel64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/sw/spack-levante/intel-oneapi-mpi-2021.5.0-mrcss7/mpi/2021.5.0/libfabric/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/on/a/road/to/nowwhere/test/run_20000101-20001231/work//lib/fesom/
-export OASIS_FFLAGS="-fPIC"
-export OASIS_CFLAGS="-fPIC"
-export CFLAGS="-fPIC"
-export CCFLAGS="-fPIC"
-export FFLAGS="-fPIC"
-export FCFLAGS="-fPIC"
-export SZIPROOT=/sw/spack-levante/libaec-1.0.5-gij7yv
-export ECCODESROOT=/work/ab0246/HPC_libraries/intel-oneapi-compilers/2022.0.1-gcc-11.2.0/openmpi/4.1.2-intel-2021.5.0
-export ESM_NETCDF_C_DIR=$NETCDFROOT
-export ESM_NETCDF_F_DIR=$NETCDFFROOT
-export OIFS_GRIB_API_INCLUDE="-I$ECCODESROOT/include"
-export OIFS_GRIB_API_LIB="-L$ECCODESROOT/lib64 -leccodes_f90 -leccodes"
-export OIFS_GRIB_INCLUDE="$OIFS_GRIB_API_INCLUDE"
-export OIFS_GRIB_LIB="$OIFS_GRIB_API_LIB"
-export OIFS_GRIB_API_BIN="$ECCODESROOT/bin"
-export LAPACK_LIB_DEFAULT="-L$MKLROOT/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
-export OIFS_OASIS_BASE=$(pwd)/oasis
-export OIFS_OASIS_INCLUDE="-I$OIFS_OASIS_BASE/build/lib/psmile -I$OIFS_OASIS_BASE/build/lib/psmile/scrip -I$OIFS_OASIS_BASE/build/lib/psmile/mct -I$OIFS_OASIS_BASE/build/lib/psmile/mct/mpeu "
-export OIFS_OASIS_LIB="-L$OIFS_OASIS_BASE/build/lib/psmile -L$OIFS_OASIS_BASE/build/lib/psmile/scrip -L$OIFS_OASIS_BASE/build/lib/psmile/mct -L$OIFS_OASIS_BASE/build/lib/psmile/mct/mpeu  -lpsmile -lmct -lmpeu -lscrip -qmkl"
-export OIFS_NETCDF_INCLUDE="-I$NETCDFROOT/include"
-export OIFS_NETCDF_LIB="-L$NETCDFROOT/lib -lnetcdf"
-export OIFS_NETCDFF_INCLUDE="-I$NETCDFFROOT/include"
-export OIFS_NETCDFF_LIB="-L$NETCDFFROOT/lib -lnetcdff"
-export OIFS_FC=$FC
-export OIFS_FFLAGS="-r8 -fp-model precise -align array32byte -O3 -qopenmp -g -traceback -convert big_endian -march=core-avx2 -mtune=core-avx2"
-export OIFS_FFIXED=""
-export OIFS_FCDEFS="BLAS LITTLE LINUX INTEGER_IS_INT"
-export OIFS_MPI_LIB="$MPI_LIB"
-export OIFS_LFLAGS="$OIFS_MPI_LIB -qopenmp"
-export OIFS_CC=$CC
-export OIFS_CFLAGS="-fp-model precise -O3 -g -traceback -qopt-report=0 -fpe0 -qopenmp -march=core-avx2 -mtune=core-avx2"
-export OIFS_CCDEFS="LINUX LITTLE INTEGER_IS_INT _ABI64 BLAS _OPENMP"
 export taken2from=fesom2_compile
 export ENVIRONMENT_SET_BY_ESMTOOLS=TRUE
 
-unset SLURM_DISTRIBUTION
-unset SLURM_NTASKS
-unset SLURM_NPROCS
-unset SLURM_ARBITRARY_NODELIST
 
-pushd rnfmap
-rm -rf bin; mkdir bin; cd src; make ; cd .. ; cp bin/rnfmap.exe ./bin/rnfma
+pushd fesom-2.6
+mkdir -p build; cd build; cmake -DCMAKE_INSTALL_PREFIX=../ ..;   make install -j `nproc --all`
 popd
